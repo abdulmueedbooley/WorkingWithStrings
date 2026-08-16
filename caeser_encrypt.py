a6540c1b1beb4fdfd21a0aba1f59f69404ee07f3
abdulmueedbooley <abdulmueed.booley@gmail.com>
2026-08-16 11:00:08 +0200

    Update caeser_encrypt.py

--- caeser_encrypt.py
+++ caeser_encrypt.py
@@ -1,15 +1,26 @@
-plaintext = input('Enter a lower case word: ')
-distance = int(input('Enter distance between letters:'))
-code = ""
-for ch in plaintext:
-    ordValue = ord(ch)
-    ciphervalue = ordValue + distance
-    #* Check if the shifted letter goes past 'z' (out of the lowercase alphabet)
-    if ciphervalue > ord('z'):
-        #* If it does wrap back around 'a' by calculating
-        #* the remaining shift distance and applying it from 'a'
-        ciphervalue = ord('a') + distance - (ord('z') - ordvalue + 1)
-    code += chr(ciphervalue)
+
+#* Booley
+plain_text = input('Enter lowercase text: ')
+#* distance = 3
+distance = int(input('Enter distance: '))
+#* Empty string to store encrypted letters
+code = ''
+#* iterating through each character and converting them into there ASCII value 
+for ch in plain_text:
+    # 'y' = 121
+    ord_Value = ord(ch)
+    # 124 = 121 + 3
+    cipher_Value = ord_Value + distance
+    #* if 124 > 122 (in this case the condition is true)
+    if cipher_Value > ord('z'):
+        #* In this method we need to wrap - around the lowercase alphabet
+        #* Within the parenthesis we find the difference between the 2 letters (ord('z') - ord_Value + 1)
+        #* Once we have found the difference we ' + 1' used as an extra step to wrap back to lowercase 'a'
+        #* The extra steps are then added to ord('a') = 97 + 3 - 2 = 98
+        #* chr(98) = 'b' ('b' is the encrypted value)
+        cipher_Value = ord('a') + distance -(ord('z') - ord_Value + 1)
+    # After each iteration the NEW encrypted letter gets stored into the empty string
+    # REMEMBER the cipher_value is still a number so dont forget to integrate the chr() function 
+    # This converts an integer to a string    
+    code += chr(cipher_Value)
 print(code)
-
-
